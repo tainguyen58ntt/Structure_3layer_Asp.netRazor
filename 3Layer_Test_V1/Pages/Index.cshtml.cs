f151445bedc4f707d16cdc917a43fef1d8973b6f
tainguyen58ntt <tainguyen58ntt@gmail.com>
2023-06-02 16:34:46 +0700

    add repo

---
+++ 3Layer_Test_V1/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Abby.Models;
+using DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _3Layer_Test_V1.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		private ApplicationDbContext _db;
+        public IEnumerable<Category> Category { get; set; }
+       
+		public IndexModel(ApplicationDbContext db) {
+			_db = db;
+		}
+		public void OnGet()
+		{
+			Category = _db.Category;
+		}
+	}
+}
---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Abby.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Category> Category { get; set; }
+	}
+}
